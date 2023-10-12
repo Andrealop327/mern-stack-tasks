85603fb9d0fd727433b61433b075e1898d9c4e17
Andrealop327 <andrefire37@gmail.com>
2023-10-12 11:17:16 -0500

    agragamos middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
+//settings
+
+app.set ('port',  process.env.PORT || 3000);
+
+// middleware
+app.use(morgan('dev'));
+app.use(express.json());  
+//routes
 
 
+app.use(require('./routes/task.route'));
+//satatic files
 
-app.listen(3000, ()=>{
-    console.log(' server on port 3000');
+// s tarting the sever
+
+app.listen(app.get('port'), ()=>{
+   console.log(`server on port ${app.get('port')}`)
 });
